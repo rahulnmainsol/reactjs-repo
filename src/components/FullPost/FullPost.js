90a1ecd00ef30e04c02d945c2c37112bb2c0e5c6
rahulnmainsol <rahul.n499@gmail.com>
2018-07-09 19:16:09 +0530

    new commit

---
+++ src/components/FullPost/FullPost.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import './FullPost.css';
+
+class FullPost extends Component {
+
+    state = {
+
+        loadedPosts : null
+    }
+
+    componentDidUpdate () {
+
+
+        if(this.props.id){
+
+            if( !this.state.loadedPosts || (this.state.loadedPosts && this.state.loadedPosts.id !== this.props.id)){
+                axios.get('/posts/'+ this.props.id)
+                .then(response => {
+                    this.setState({loadedPosts: response.data});
+                    // console.log(reponse);
+                });
+            }
+            
+        }
+    }
+
+    postDeleteHandler = () => {
+        axios.delete('/posts/'+ this.props.id)
+            .then(response => {
+                console.log(response);
+            });
+    }
+
+    render () {
+        let post = <p style={{textAlign:'center'}}>Please select a Post!</p>;
+        if(this.props.id){
+            post = <p style={{textAlign:'center'}}>Loading....</p>;
+        }
+        if(this.state.loadedPosts){
+
+            post = (
+                <div className="FullPost">
+                    <h1>{this.state.loadedPosts.title}</h1>
+                    <p>{this.state.loadedPosts.body}</p>
+                    <div className="Edit">
+                        <button onClick={this.postDeleteHandler}>Delete</button>
+                    </div>
+                </div>
+    
+            );
+        }
+        
+        return post;
+    }
+}
+
+export default FullPost;
---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+
+import axios from 'axios';
+
+import Post from '../../components/Post/Post';
+import FullPost from '../../components/FullPost/FullPost';
+import NewPost from '../../components/NewPost/NewPost';
+import './Blog.css';
+
+class Blog extends Component {
+
+    state ={
+        posts: [],
+        selectedPostId : null,
+        getError: false
+    }
+
+    componentDidMount(){
+
+        axios.get('/posts')
+            .then( response =>{
+                const posts = response.data.slice(0,4);
+                const updatedPosts = posts.map(post =>{
+                   return{
+                       ...post,
+                       author : 'Max'
+                   }
+                })
+                this.setState({posts:updatedPosts});
+                // console.log(response);
+            })
+            .catch(errors =>{
+                this.setState({getError:true});
+            });
+
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    }
+
+    render () {
+
+        let posts = <p>Something went wrong</p>;
+
+        if(!this.state.getError){
+            posts = this.state.posts.map( post =>{
+                return <Post key={post.id} 
+                            title={post.title} 
+                            author={post.author}
+                            clicked={() => this.postSelectedHandler(post.id) }/> ;
+            })
+        }
+
+
+        return (
+            <div>
+                <section className="Posts">
+                    
+                    {posts}
+                </section>
+                <section>
+                    <FullPost id={this.state.selectedPostId}/>
+                </section>
+                <section>
+                    <NewPost />
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Blog;
